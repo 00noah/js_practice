0dddb6fa9b151d9812d7483d64219a5585eeb5e1
Courtney Schild and Noah Horwitz <courtney@>
2019-09-04 20:53:33 -0700

    add talk.html page

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// peek-a-boo
 $(document).ready(function() {
   $(".clickable2").click(function() {
     $("#walrus-showing").fadeIn();
@@ -7,22 +8,52 @@ $(document).ready(function() {
     $("#walrus-hidden").fadeIn();
     $("#walrus-showing").fadeOut();
   });
-    // this is blank space
-    $(".top-left").click(function() {
-      $(".top-left").removeClass();
-      $(".top-left").addClass("yellow-card")
-      $(".top-center").fadeToggle();
+  // hide-and-seek
+  $(".top-left").click(function() {
+    $(".top-left").removeClass();
+    $(".top-left").addClass("yellow-card")
+    $(".top-center").fadeToggle();
   });
-      // this is blank space
-      $("button#lights-on").click(function() {
-        $("body").removeClass();
-        $("body").addClass("lights-on");
-      });
+  $("button#lights-on").click(function() {
+    $("body").removeClass();
+    $("body").addClass("lights-on");
+  });
 
-      $("button#lights-off").click(function() {
-        $("body").removeClass();
-        $("body").addClass("lights-off");
-      });
+  $("button#lights-off").click(function() {
+    $("body").removeClass();
+    $("body").addClass("lights-off");
+  });
+  // talk
+  $("button#hello").click(function() {
+    $("ul#user").prepend("<li>Hello!</li>");
+    $("ul#webpage").prepend("<li>Why hello there!</li>");
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
+  });
 
+  $("button#goodbye").click(function() {
+    $("ul#user").prepend("<li>Goodbye!</li>");
+    $("ul#webpage").prepend("<li>Goodbye, dear user!</li>");
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
+  });
 
+  $("button#stop").click(function() {
+    $("ul#user").prepend("<li>Stop copying me!</li>");
+    $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</li>");
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
+  });
 });
